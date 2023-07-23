d2d3dee5e7cba34ad97160e1724a3de92cd5fce4
Partha04 <partha16896@gmail.com>
2023-07-24 00:08:58 +0530

    [dev] create theme context with color palettes

---
+++ src/theme/colors/colors.ts
@@ -0,0 +1,20 @@
+export type Pallet = {
+  primary: string;
+  secondary: string;
+  text:string;
+  background:string;
+};
+
+export const Light: Pallet = {
+  primary: "#d85bf3",
+  secondary: "#33a4db",
+  text:"#2f2f2f",
+  background: "#d7d7d7"
+};
+
+export const Dark: Pallet = {
+  primary: "#5f91c8",
+  secondary: "#743ea9",
+  text:"#cbcbcb",
+  background:"#233347"
+};
