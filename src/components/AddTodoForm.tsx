9a6221c2cebe50603dec194a4cf554dd93b5c0a9
Partha04 <partha16896@gmail.com>
2023-07-26 13:19:35 +0530

    [dev] use loader for add api call

--- src/components/AddTodoForm.tsx
+++ src/components/AddTodoForm.tsx
@@ -4,21 +4,34 @@ import {addTodo} from "../api/TodoApi";
 import Form from "./form/Form";
 import Button from "./form/Button";
 import Input from "./form/Input";
+import Loader from "./form/Loader";
+import {ThemeContext} from "../contexts/ThemeContext";
 
 const AddTodoForm = () => {
     const {showNotification} = useContext(TodoContext);
     const [task, setTask] = useState<string>("");
+    const [loading, setLoading] = useState<boolean>(false);
+    const {pallet} = useContext(ThemeContext)
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         console.log("addTodoForm");
+        setLoading(true)
         addTodo(task)
             .then(() => {
-                showNotification("Added new task");
+                showNotification({
+                    text: "Added new task",
+                    type: "success"
+                });
+                setLoading(false);
             })
             .catch((err) => {
                 console.log(err)
-                showNotification("Failed to add task");
+                showNotification({
+                    text: "Failed to add task",
+                    type: "warning"
+                });
+                setLoading(false);
             });
     };
 
@@ -29,13 +42,22 @@ const AddTodoForm = () => {
                 name="task"
                 value={task}
                 required
+                styleProps={{
+                    width: "20rem"
+                }}
                 onChange={(value) => {
                     setTask(value);
                 }}
             />
             <Button
+                styleProps={{
+                    width: "7rem",
+                    height: "2rem",
+                    background: pallet.primary,
+                    color:pallet.text
+                }}
                 type="submit">
-                Add Task
+                {loading ? <Loader/> : "Add Task"}
             </Button>
         </Form>
     );
