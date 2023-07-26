d608401b49f4f19a819dae12fb7bc1caf8cd7487
Partha04 <partha16896@gmail.com>
2023-07-26 13:18:52 +0530

    [dev] change the style of the notifications

--- src/contexts/TodoContext.tsx
+++ src/contexts/TodoContext.tsx
@@ -1,39 +1,75 @@
-import React, {createContext, useState} from "react";
+import React, {createContext, useContext, useState} from "react";
+import {ThemeContext} from "./ThemeContext";
+
+type TodoProviderProps = {
+    children: React.ReactNode;
+};
+type NotificationProps = {
+    text: string;
+    type?: "info" | "warning" | "danger" | "success"
+}
 
 type TodoContextType = {
-  showNotification: (text: string) => void;
+    showNotification: (props: NotificationProps) => void;
 };
 
 export const TodoContext = createContext<TodoContextType>({
-  showNotification: () => {},
+    showNotification: () => {
+    },
 });
 
-type TodoProviderProps = {
-  children: React.ReactNode;
-};
 
-export const TodoProvider = ({ children }: TodoProviderProps) => {
-  const [openNotifications, setOpenNotifications] = useState<boolean>(false);
-  const [notificationsMessage, setNotificationsMessage] = useState<string>("");
+export const TodoProvider = ({children}: TodoProviderProps) => {
+    const [openNotifications, setOpenNotifications] = useState<boolean>(false);
+    const [notificationsMessage, setNotificationsMessage] = useState<string>("");
+    const {pallet} = useContext(ThemeContext);
+    const [notificationColor, setNotificationColor] = useState<string>(pallet.primary)
+    const showNotification = ({text, type}: NotificationProps) => {
+        setNotificationsMessage(text);
+        switch (type) {
+            case "danger":
+                setNotificationColor(pallet.danger)
+                break
+            case "warning":
+                setNotificationColor(pallet.warning)
+                break
+            case "success":
+                setNotificationColor(pallet.success)
+                break
+            case "info":
+                setNotificationColor(pallet.primary)
+                break;
+            default:
+                setNotificationColor(pallet.background)
+        }
+        console.log("here");
+        setOpenNotifications(true);
+        setTimeout(() => {
+            setOpenNotifications(false);
+            setNotificationsMessage("");
+        }, 3000);
+    };
 
-  const showNotification = (text: string) => {
-    setNotificationsMessage(text);
-    console.log("here");
-    setOpenNotifications(true);
-    setTimeout(() => {
-      setOpenNotifications(false);
-      setNotificationsMessage("");
-    }, 3000);
-  };
-
-  return (
-    <TodoContext.Provider
-      value={{
-        showNotification,
-      }}
-    >
-      {children}
-      {openNotifications && <p>{notificationsMessage}</p>}
-    </TodoContext.Provider>
-  );
+    return (
+        <TodoContext.Provider
+            value={{
+                showNotification,
+            }}
+        >
+            {children}
+            {openNotifications && <div style={
+                {
+                    position: "fixed",
+                    padding: "0.5rem 1rem",
+                    top: "2rem",
+                    right: "2rem",
+                    borderRadius: "0.3rem",
+                    backgroundColor: `${notificationColor}`,
+                    animation: 'slideInAndOut 1s linear forwards',
+                    color: "#d2d2d2",
+                    fontWeight:"bold"
+                }
+            }>{notificationsMessage}</div>}
+        </TodoContext.Provider>
+    );
 };
--- src/theme/colors/colors.ts
+++ src/theme/colors/colors.ts
@@ -1,20 +1,33 @@
 export type Pallet = {
-  primary: string;
-  secondary: string;
-  text:string;
-  background:string;
+    primary: string;
+    secondary: string;
+    success:string,
+    warning: string,
+    danger: string,
+    text: string;
+    background: string;
+    paper: string
 };
 
 export const Light: Pallet = {
-  primary: "#d85bf3",
-  secondary: "#33a4db",
-  text:"#2f2f2f",
-  background: "#d7d7d7"
+    primary: "#d85bf3",
+    secondary: "#33a4db",
+    success:"#0e9614",
+    warning: "#c38528",
+    danger: "#cd1936",
+    text: "#2f2f2f",
+    background: "#d7d7d7",
+    paper: "#c6c6c6"
+
 };
 
 export const Dark: Pallet = {
-  primary: "#5f91c8",
-  secondary: "#743ea9",
-  text:"#cbcbcb",
-  background:"#233347"
+    primary: "#5f91c8",
+    secondary: "#743ea9",
+    success:"#0e9614",
+    warning: "#c38528",
+    danger: "#cd1936",
+    text: "#cbcbcb",
+    background: "#233347",
+    paper: "#0d0951"
 };
