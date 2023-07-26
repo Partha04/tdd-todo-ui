78d7190e4f0887097fd1231759c19034cbb37ba0
Partha04 <partha16896@gmail.com>
2023-07-26 11:34:51 +0530

    [dev] should add a task when new task is added

--- src/__test__/components/AddTodoForm.test.tsx
+++ src/__test__/components/AddTodoForm.test.tsx
@@ -1,10 +1,38 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import AddTodoForm from "../../components/AddTodoForm";
+import { TodoProvider } from "../../contexts/TodoContext";
+import { server } from "../../mocks/server";
+import React from "react";
 
 describe("Add todo form", () => {
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
+  afterEach(() => server.resetHandlers());
   test("should render todo form", async () => {
     render(<AddTodoForm />);
     const inputText = await screen.findByRole("textbox");
     expect(inputText).toBeInTheDocument();
+    const submitButton = await screen.findByRole("button");
+    expect(submitButton).toBeInTheDocument();
+  });
+  test("should add new todo with test task 1 ", async () => {
+    render(
+      <TodoProvider>
+        <AddTodoForm />
+      </TodoProvider>
+    );
+    const inputText = await screen.findByRole("textbox");
+    const submitButton = await screen.findByRole("button");
+    fireEvent.change(inputText, {
+      target: {
+        value: "Task 1",
+      },
+    });
+    fireEvent.click(submitButton);
+    const message = await screen.findByText("Added new task");
+    expect(message).toBeInTheDocument();
+    setTimeout(() => {
+      expect(message).not.toBeInTheDocument();
+    }, 3000);
   });
 });
--- src/components/AddTodoForm.tsx
+++ src/components/AddTodoForm.tsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React, {useContext, useState} from "react";
+import {TodoContext} from "../contexts/TodoContext";
+import {addTodo} from "../api/TodoApi";
+import Form from "./form/Form";
+import Button from "./form/Button";
+import Input from "./form/Input";
 
-type Props = {};
+const AddTodoForm = () => {
+    const {showNotification} = useContext(TodoContext);
+    const [task, setTask] = useState<string>("");
 
-const AddTodoForm = (props: Props) => {
-  return <input />;
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log("addTodoForm");
+        addTodo(task)
+            .then(() => {
+                showNotification("Added new task");
+            })
+            .catch((err) => {
+                console.log(err)
+                showNotification("Failed to add task");
+            });
+    };
+
+
+    return (
+        <Form data-testid="addTodoForm" onSubmit={(e) => handleSubmit(e)}>
+            <Input
+                name="task"
+                value={task}
+                required
+                onChange={(value) => {
+                    setTask(value);
+                }}
+            />
+            <Button
+                type="submit">
+                Add Task
+            </Button>
+        </Form>
+    );
 };
 
 export default AddTodoForm;
